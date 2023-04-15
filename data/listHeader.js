a602cdcd520ea054bce5383c39d02382f79f0537
Dat Nguyen <nguyentiendat3900@gmail.com>
2023-04-16 00:16:53 +0700

    update navbar

---
+++ data/listHeader.js
@@ -0,0 +1,58 @@
+const LIST_DATA = [
+  {
+    id: 1,
+    title: "About This Place",
+    children: [
+      "Banff National Park",
+      "Town & Village",
+      "Ski Resorts",
+      "History & Heritage",
+      "Seasons & Climate",
+      "Wildlife",
+      "Stories & Blog",
+    ],
+    images: [
+      "assets/image/AboutThisPlace/Place1.jpg",
+      "assets/image/AboutThisPlace/Place2.jpg",
+      "assets/image/AboutThisPlace/Place3.jpg",
+    ],
+  },
+  {
+    id: 2,
+    title: "Visitor Info",
+    children: [
+      "Getting Here",
+      "Getting Around",
+      "Travel Information",
+      "Weather & Webcams",
+      "Seasons & Climate",
+      "Wildlife",
+      "Stories & Blog",
+    ],
+    images: [
+      " assets/image/VisitorInfo/VisitorInfo1.jpg",
+      " assets/image/VisitorInfo/VisitorInfo2.jpg",
+      " assets/image/VisitorInfo/VisitorInfo3.jpg",
+    ],
+  },
+  {
+    id: 3,
+    title: "Things to Do",
+    children: [
+      "Sightseeing",
+      "Adventure & Outdoors",
+      "Arts & Culture",
+      "Eat & Drink",
+      "Wellness",
+      "Shopping",
+      "Weddings",
+      "Events & Festivals",
+    ],
+    images: [
+      "assets/image/ThingsToDo/ThingsToDo1.jpg",
+      "assets/image/ThingsToDo/ThingsToDo2.jpg",
+      "assets/image/ThingsToDo/ThingsToDo3.jpg",
+    ],
+  },
+];
+export default LIST_DATA;
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import LIST_DATA from "./data/listHeader.js";
 const buttonNavBarMenu = document.querySelector(".navbar_button_menu");
 const navBarIconCenter = document.querySelector(".icon-bars-center");
 const navBarIconTop = document.querySelector(".icon-bars-top");
@@ -6,12 +7,36 @@ const modalMenuHeader = document.querySelector(".modal-menu");
 const mainHeader = document.querySelector(".main_header");
 const iconSearch = document.querySelector(".icon-svg-search");
 const iconBookmarks = document.querySelector(".icon-svg-save");
+const navbarListMobile = document.querySelector(".modal-menu-list");
+let navBarTitleMobiles;
 const app = {
-  render() {},
+  render() {
+    //render navbar in mobile
+    let templateNavbarListMobile = LIST_DATA.map(
+      (item) => `<li class="modal-menu-item">
+      <button class="modal-menu-item-button">${item.title}</button>
+      <div class="content-nav-mobile">
+      <ul class="content-nav-mobile-list">
+      ${item.children
+        .map(
+          (content) => `<li class="content-nav-mobile-item">
+          <a class="content-nav-mobile-item-link" href="">${content}</a>
+          </li>`
+        )
+        .join("")}
+          </ul>
+          </div>
+          </li>`
+    );
+    templateNavbarListMobile = templateNavbarListMobile.join("");
+    navbarListMobile.insertAdjacentHTML("afterbegin", templateNavbarListMobile);
+    navBarTitleMobiles = document.querySelectorAll(".modal-menu-item");
+  },
   handleEvent() {
     // handle click show header
     if (buttonNavBarMenu) {
       buttonNavBarMenu.onclick = () => {
+        console.log(1);
         modalMenuHeader.classList.toggle("hide-on-pc");
         mainHeader.style.backgroundColor = "#fff";
         navBarIconTop.classList.toggle("transition");
@@ -24,6 +49,32 @@ const app = {
           document.body.style = "overflow:hidden";
         }
       };
+    }
+    //handle click show content navbar header
+    if (navBarTitleMobiles) {
+      navBarTitleMobiles.forEach((element) => {
+        console.log(element);
+        element.onclick = (e) => {
+          const content = element.querySelector(".content-nav-mobile");
+          const listItems = element.querySelectorAll(
+            ".content-nav-mobile-item"
+          );
+          const navBarTitleMobilesIcon = element.querySelector(
+            ".modal-menu-item-button"
+          );
+          console.log(content);
+          if (content) {
+            console.log(1);
+            content.classList.toggle("isShow");
+            navBarTitleMobilesIcon.classList.toggle("isChange");
+            listItems.forEach((item) => {
+              setTimeout(() => {
+                item.classList.toggle("isShow");
+              }, 100);
+            });
+          }
+        };
+      });
     }
   },
   start() {
